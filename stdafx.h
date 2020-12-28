a10a411a43b5fc08d039de7f35839ae10647ca58
Kostize <kostik_563@mail.ru>
2020-12-28 17:53:26 +0300

    Добавлен файл конфигурации и

---
+++ stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+
+
+#pragma once
+
+#include "targetver.h"
+#include <iomanip>
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream> 
+#include <vector> 
+#include <list> 
+#include <string> 
+#include <numeric>
+#include <locale>
+#include <clocale>
+#include "Getf.h" 
+using namespace std;
+
+
+// TODO: reference additional headers your program requires here
